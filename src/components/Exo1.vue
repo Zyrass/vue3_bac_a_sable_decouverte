c222bbdb87f8e6f7c0339ceae1b9e3cf7ff8d38d
Zyrass <zyrass@outlook.fr>
2022-03-24 21:53:59 +0100

    Inversement de l'affichage du template et du script

--- src/components/Exo1.vue
+++ src/components/Exo1.vue
@@ -1,39 +1,3 @@
-<script setup lang="ts">
-import { ref, onMounted } from 'vue';
-
-const TITLE = 'EXO 1';
-
-// Configuration
-const counter = ref(0);
-const solde = ref(15000);
-
-let message = ref('');
-
-// Fonction pour retirer de l'argent.
-function retrait(n: number) {
-  if (solde.value > 0) {
-    counter.value++;
-    solde.value -= n;
-    message.value = `Retrait de ${n} € effectué avec succès`;
-    if (solde.value < 0) {
-      solde.value = 0;
-    }
-  } else {
-    message.value = 'Solde insuffisant';
-  }
-}
-
-// Vérification au démarrage de la valeur du compteur.
-onMounted(() => {
-  console.log(
-    `Le solde à l'initialisation de l'application est fixé à ${solde.value}`
-  );
-  console.log(
-    `Le nombre d'opération à l'initialisation de l'application est fixé à ${counter.value}`
-  );
-});
-</script>
-
 <template>
   <fieldset>
     <legend>{{ TITLE }}</legend>
@@ -60,6 +24,44 @@ onMounted(() => {
     <p>{{ message }}</p>
   </fieldset>
 </template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+
+const TITLE = 'EXO 1';
+
+// Configuration
+const counter = ref(0);
+const solde = ref(1000);
+const operation = ref([]);
+
+let message = ref('');
+
+// Fonction pour retirer de l'argent.
+function retrait(n: number) {
+  if (solde.value > 0) {
+    counter.value++;
+    solde.value -= n;
+
+    message.value = `Retrait de ${n} € effectué avec succès`;
+    if (solde.value < 0) {
+      solde.value = 0;
+    }
+  } else {
+    message.value = 'Solde insuffisant';
+  }
+}
+
+// Vérification au démarrage de la valeur du compteur.
+onMounted(() => {
+  console.log(
+    `Le solde à l'initialisation de l'application est fixé à ${solde.value}`
+  );
+  console.log(
+    `Le nombre d'opération à l'initialisation de l'application est fixé à ${counter.value}`
+  );
+});
+</script>
 
 <style scoped lang="scss">
 fieldset {
